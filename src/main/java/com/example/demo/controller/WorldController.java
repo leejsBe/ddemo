997d8f866c37cee4d557abe13f8de6fd333c21b6
jameslee <bcm.jameslee@gmail.com>
2019-04-16 17:28:51 +0900

    efgg

---
+++ src/main/java/com/example/demo/controller/WorldController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WorldController {
+
+	
+	@RequestMapping(value = {"/main/main", "/mobile/main/main"}, method = RequestMethod.GET)
+	public String mainPage(Model model,HttpServletRequest request) {
+		System.out.println("efefef");
+		return "main/mainPage";
+	}
+	
+}
