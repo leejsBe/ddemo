10c33eb6d6b7ff0e669e572e92755dd5994f0dfc
jameslee <bcm.jameslee@gmail.com>
2019-04-16 13:50:03 +0900

    test

---
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+		@RequestMapping("/")
+		public String index() {
+			return "hello world!";
+		}
+		
+		@RequestMapping("/test")
+		public String test() {
+			return "hello world~~~!!!!";
+		}
+	
+}
